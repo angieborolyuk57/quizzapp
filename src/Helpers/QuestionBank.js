20df4c986a3b4559eb182d8a29696307e59e1bc2
angieborolyuk57 <116447706+angieborolyuk57@users.noreply.github.com>
2023-12-04 23:04:15 +0100

    quiz files

---
+++ src/Helpers/QuestionBank.js
@@ -0,0 +1,61 @@
+export const Questions = [
+    {
+        prompt: 'What is 1+3?',
+        optionA:'7',
+        optionB:'10',
+        optionC:'4',
+        optionD:'2',
+        answer:"C",
+        },
+        {
+          prompt: 'What is 10 + 13 ?',
+          optionA:'10',
+          optionB:'20',
+          optionC:'23',
+          optionD:'30',
+          answer:"C",
+          },
+          {
+            prompt: 'What is 100 - 50 ?',
+            optionA:'7',
+            optionB:'33',
+            optionC:'50',
+            optionD:'99',
+            answer:"C",
+            },
+            {
+              prompt: 'What is 99 + 1 ?',
+              optionA:'79',
+              optionB:'24',
+              optionC:'100',
+              optionD:'56',
+              answer:"C",
+              },
+
+
+        /* 
+        {
+            prompt: "What is my name?",
+            optionA: "John",
+            optionB: "Jake",
+            optionC: "Josh",
+            optionD: "Pedro",
+            answer: "D",
+          },
+          {
+            prompt: "Which of this is not a programming language?",
+            optionA: "Python",
+            optionB: "JavaScript",
+            optionC: "MC-03",
+            optionD: "Java",
+            answer: "C",
+          },
+          {
+            prompt: "Which of this is not a javascript framework?",
+            optionA: "React",
+            optionB: "Angular",
+            optionC: "Vue",
+            optionD: "Java",
+            answer: "D",
+          }, */
+];
